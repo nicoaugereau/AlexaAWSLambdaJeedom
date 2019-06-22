3789c626b2be0416d6b855cbc36ed473cff14167
nicoaugereau <32208373+nicoaugereau@users.noreply.github.com>
2019-06-22 22:57:01 +0200

    new config

--- config.js
+++ config.js
@@ -9,17 +9,52 @@ module.exports = {
     },
     cmds: [
         {
-            place: 'salon',
+            description: '1 lumière du salon',
+            place: 'salon_lightIntent',
+            intent: 'lightIntent',
             cmd: {
-                'Allume': 86,
-                'Eteins': 87,
+                'allume': 86,
+                'éteins': 87,
+                'état': 85,
+            }
+        },
+        {
+            description: 'fenêtres du salon',
+            place: 'salon_windowIntent',
+            intent: 'windowIntent',
+            cmd: {
+                'ouvre': 86,
+                'ferme': 87,
+                'état': 85,
+            }
+        },
+        {
+            description: 'prises du salon',
+            place: 'salon_wallPlugIntent',
+            intent: 'wallPlugIntent',
+            cmd: {
+                'allume': 86,
+                'éteins': 87,
+                'état': 85,
             }
         },
         {
             place: 'salle de bain',
+            intent: 'windowIntent',
             cmd: {
-                'Allume': 266,
-                'Eteins': 267,
+                'on': 266,
+                'off': 267,
+                'state': 268,
+            }
+        },
+        {
+            description: 'toutes les lumières',
+            place: 'maison_wallPlugIntent',
+            intent: 'wallPlugIntent',
+            cmd: {
+                'allume': 86,
+                'éteins': 87,
+                'état': 85,
             }
         }
     ]
