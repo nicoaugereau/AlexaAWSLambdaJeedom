28f8bc3889eee3fad618902c943e44f7a5d411ac
Nicolas <32208373+nicoaugereau@users.noreply.github.com>
2019-11-25 22:04:26 +0100

    code rework and adding features
    code rework
    adding language support to come
    adding features to come

--- index.js
+++ index.js
@@ -1,89 +1,115 @@
+/*
+    Alexa Skill Interaction for Jeedom
+    Skill Version : 1.0
+    Author : Nicolas Augereau
+    Licence : MIT
+*/
+
 'use strict';
 
 const config = require('./config');
 const request = require('./request');
 
-const messages = {
-    WELCOME: 'Welcome to Freebox Devialet Assistant',
+const locale = event.request.locale;
+
+const fr_FR = {
+    WELCOME: 'Bienvenue sur l\'assistant Freebox Devialet',
     WHAT_DO_YOU_WANT: 'Que vouliez-vous dire ?',
     UNHANDLED: 'Désolé cette skill n\'est pas prise en charge.',
-    HELP: 'Désolée je n\'ai pas pu répondre à votre demande. Vous pouvez me demander par exemple : Alexa demande à Devialer d\allumer la lumière de la cuisine, ou ferme le volet du salon, ou encore baisse le volet à 20%.',
-    STOP: 'Bye! Thanks for using the Freebox Devialet Assistant!',
+    HELP: 'Désolée je n\'ai pas pu répondre à votre demande. Vous pouvez me demander par exemple : Alexa demande à Devialer d\'allumer la lumière de la cuisine, ou ferme le volet du salon, ou encore baisse le volet à 20%.',
+    STOP: 'Au revoir. Merci pour avoir utilisé l\'assistant Freebox Devialet !',
     ERROR: 'Désolé il y a eu une erreur',
     ERROR_ACTION: 'désolé je n\'ai pas compris l\'action',
     ERROR_PLACE: 'désolé je n\'ai pas compris l\'emplacement',
     ERROR_ACTION_SCENARIO: 'désolé l\'action n\'a pas été trouvée. Vous pouvez demander stoppe, arrête, lance, exécute, démarre, active, désactive.',
     ERROR_SCENARIO_ID: 'désoélé le scenario n\'a pa été trouvé'
 };
+const en_GB = {
+    WELCOME: 'Welcome to Freebox Devialet Assistant',
+    WHAT_DO_YOU_WANT: 'What did you want to say?',
+    UNHANDLED: 'Désolé cette skill n\'est pas prise en charge.',
+    HELP: 'Désolée je n\'ai pas pu répondre à votre demande. Vous pouvez me demander par exemple : Alexa demande à Devialer d\'allumer la lumière de la cuisine, ou ferme le volet du salon, ou encore baisse le volet à 20%.',
+    STOP: 'Bye! Thanks for using the Freebox Devialet Assistant!',
+    ERROR: 'Désolé il y a eu une erreur',
+    ERROR_ACTION: 'sorry i did not understand the action',
+    ERROR_PLACE: 'sory i did not understand the place',
+    ERROR_ACTION_SCENARIO: 'désolé l\'action n\'a pas été trouvée. Vous pouvez demander stoppe, arrête, lance, exécute, démarre, active, désactive.',
+    ERROR_SCENARIO_ID: 'sorry i did not find the scenario'
+};
 
-//-----------------------------------------------------------------------------
-
-function getCommand(place, action, intent)
+function getRequest(type, action, intent, place)
 {
-    console.log("Getting Jeedom object information");
-	let room = config.cmds.find((t) => t.place == place + '_' + intent);
-	console.log("getCommand function. Room = " + room + " action = " + action);
-	if (room && room.cmd && room.cmd[action])
-		return Promise.resolve(room.cmd[action]);
-
-	return Promise.reject('désolé l\'emplacement ' + place + ' et l\'intention ' + intent +' ne prennent pas en charge l\'action ' + action);
-}
-
-function doRequest(id, type, slider, json = true)
-{
-    console.log("Getting command request");
-    if (slider) {
-        console.log("Getting command request with slider value. Id = " + id + " type =  " + type + " slider = " + slider);
-        return request({
-            host: config.jeedom.host,
-            port: config.jeedom.port,
-            path: config.jeedom.path,
-            json
-            }, {
-             'apikey': config.jeedom.apikey,
-             'type': type,
-             'id': id,
-             'slider': slider
-        });
-    }else{
-        console.log("Getting command request without slider value. Id = " + id + " type =  " + type);
-        return request({
-            host: config.jeedom.host,
-            port: config.jeedom.port,
-            path: config.jeedom.path,
-            json
-            }, {
-            'apikey': config.jeedom.apikey,
-            'type': type,
-            'id': id
-        });
+    console.log("Getting Jeedom object information from Alexa JSON");
+    // get informations from config.js
+    switch (type) {
+        case "scenario":
+            console.log("Getting Jeedom scenario informations");
+            let scenarioId = config.scenarios.find((t) => t.id == intent);
+            if (scenarioId && scenarioId.scenario && scenarioId.scenario[action])
+                return Promise.resolve(scenarioId.scenario[action]);
+            
+            return Promise.reject('désolé le scenario ' + id + ' ne prend pas en charge l\'action ' + action);
+        case "cmd":
+            let room = config.cmds.find((t) => t.place == place + '_' + intent);
+            console.log("getCommand function. action = " + action + " place = " + place + " intent = " + intent);
+            if (room && room.cmd && room.cmd[action])
+                return Promise.resolve(room.cmd[action]);
+            
+            return Promise.reject('désolé l\'emplacement ' + place + ' et l\'intention ' + intent +' ne prennent pas en charge l\'action ' + action);
+        case "interact":
+        case "variable":
     }
+    
 }
 
-function getScenario(id, action) 
+function jeeQuery(type, id, value, json = true)
 {
-    console.log("Getting Jeedom scenario information");
-    let scenarioId = config.scenarios.find((t) => t.id == id);
-    if (scenarioId && scenarioId.scenario && scenarioId.scenario[action])
-		return Promise.resolve(scenarioId.scenario[action]);
-
-    return Promise.reject('désolé le scenario ' + id + ' ne prend pas en charge l\'action ' + action);
-}
-
-function doRequestScn(action, id, type, json = true)
-{
-    console.log("Getting scenario request. Id = " + id + " action " + action + " type = " + type);
-	return request({
-		host: config.jeedom.host,
-		port: config.jeedom.port,
-		path: config.jeedom.path,
-		json
-		}, {
-		 'apikey': config.jeedom.apikey,
-		 'type': type,
-		 'id': id,
-		 'action': action
-	});
+    console.log("Getting command request for Jeedom");
+    // Jeedom Query
+    switch (type) {
+        case "scenario":
+            console.log("Getting scenario request. Id = " + id + " action " + value + " type = " + type);
+            return request({
+                host: config.jeedom.host,
+                port: config.jeedom.port,
+                path: config.jeedom.path,
+                json
+                }, {
+                    'apikey': config.jeedom.apikey,
+                    'type': type,
+                    'id': id,
+                    'action': value
+            });
+        case "cmd":
+            if (slider) {
+            console.log("Getting command request with slider value. Id = " + id + " type =  " + type + " slider = " + value);
+            return request({
+                host: config.jeedom.host,
+                port: config.jeedom.port,
+                path: config.jeedom.path,
+                json
+                }, {
+                    'apikey': config.jeedom.apikey,
+                    'type': type,
+                    'id': id,
+                    'slider': value
+                });
+            }else{
+                console.log("Getting command request without slider value. Id = " + id + " type =  " + type);
+                return request({
+                    host: config.jeedom.host,
+                    port: config.jeedom.port,
+                    path: config.jeedom.path,
+                    json
+                }, {
+                    'apikey': config.jeedom.apikey,
+                    'type': type,
+                    'id': id
+                });
+            }
+        case "interact":
+        case "variable":
+    }
 }
 
 function createResponse(text, shouldEndSession = true) {
@@ -114,15 +140,14 @@ function handleRequest(intent) {
     let slider = null;
     let scenarioId = null;
     let reqType = null;
-    
+
     switch (intentName) {
         case "light":
         case "door":
         case "window":
         case "curtain":
         case "shutter":
-	    console.log("Getting intent name = " + intentName);
-	        
+	        console.log("Getting intent name = " + intentName);
             reqType = 'cmd';
 
 	        try {
@@ -140,29 +165,27 @@ function handleRequest(intent) {
 		        }
 		        if (sliderValue) {
                     console.log("Slider value = " + sliderValue);
-                        slider = 100-sliderValue;
-                    	console.log("slider = " + slider);
+                    slider = 100-sliderValue;
+                    console.log("slider = " + slider);
 		        }
 	        }
 	        
 	        catch (err) {
-		        return Promise.reject(messages.ERROR);
+		        return Promise.reject(locale.ERROR);
 	        }
-
 	        if (action == null) {
-		        return Promise.reject(messages.ERROR_ACTION);
+		        return Promise.reject(locale.ERROR_ACTION);
 	        } else if (place == null) {
-                return Promise.reject(messages.ERROR_PLACE);
+                return Promise.reject(locale.ERROR_PLACE);
             }
             
-            return getCommand(place, action, intentName)
+            return getRequest(reqType, action, intentName, place)
                 .then(console.log("Sending object command and request"))
-                .then((c) => doRequest(c, reqType, slider, false))
+                .then((c) => jeeQuery(reqType, c, slider, false))
 				.then(() => createResponse([ "très bien", "oui", "compris", "je m'en occupe" ][Math.floor(Math.random() * 4)]) );
         break;
         case "wallplug":
             console.log("Getting intent name = " + intentName);
-	        
             reqType = 'cmd';
 
 	        try {
@@ -180,24 +203,21 @@ function handleRequest(intent) {
 	        }
 	        
 	        catch (err) {
-		        return Promise.reject(messages.ERROR);
+		        return Promise.reject(locale.ERROR);
 	        }
-
 	        if (action == null) {
-		        return Promise.reject(messages.ERROR_ACTION);
+		        return Promise.reject(locale.ERROR_ACTION);
 	        } else if (place == null) {
-                return Promise.reject(messages.ERROR_PLACE);
+                return Promise.reject(locale.ERROR_PLACE);
             }
             
-            return getCommand(place, action, intentName)
+            return getRequest(reqType, action, intentName, place)
                 .then(console.log("Sending object command and request"))
-                .then((c) => doRequest(c, reqType, slider, false))
+                .then((c) => jeeQuery(reqType, c, slider, false))
 				.then(() => createResponse([ "très bien", "oui", "compris", "je m'en occupe" ][Math.floor(Math.random() * 4)]) );
         break;
         case "scenario":
-            
             console.log("Getting intent name = " + intentName);
-        	
             reqType = 'scenario';
             
             try {
@@ -206,7 +226,7 @@ function handleRequest(intent) {
             	}
             }
             catch (err) {
-            	return Promise.reject(messages.ERROR_SCENARIO_ID);
+            	return Promise.reject(locale.ERROR_SCENARIO_ID);
     		}
 
             try {
@@ -217,27 +237,27 @@ function handleRequest(intent) {
 		        }
 	        }
 	        catch (err) {
-		        return Promise.reject(messages.ERROR_ACTION_SCENARIO);
+		        return Promise.reject(locale.ERROR_ACTION_SCENARIO);
             }
             
             
-            return getScenario(scenarioId, action)
+            return getRequest(reqType, action, intentName, place)
                 .then(console.log("Sending scenario command and request"))
-                .then((c) => doRequestScn(c, scenarioId, reqType, false))
+                .then((c) => jeeQuery(reqType, scenarioId, c, false))
 				.then(() => createResponse([ "très bien", "oui", "compris", "je m'en occupe" ][Math.floor(Math.random() * 4)]) );
         break;
         default:
-            context.succeed(createResponse(messages.H));
+            context.succeed(createResponse(locale.HELP));
     }
     console.log("End dialog with Alexa Freebox Devialet and Jeedom");
 }
 
 exports.handler = (event, context, callback) => {
-	
+    
     switch (event.request.type) {
         case "LaunchRequest":
             console.log("Openning Freebox Devialet");
-            context.succeed(createResponse("Welcome to Freebox Devialet Assistant.", false));
+            context.succeed(createResponse(locale.WELCOME, false));
         break;
         case "IntentRequest":
             console.log("Starting dialog with Alexa Freebox Devialet and Jeedom");
